7bd0ba111191909e4febf08b529760ff5fadf28d
Jake Robbins <jrobbinsd@gmail.com>
2017-11-09 09:46:46 -0700

    turn in commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../db/knex')
+// var Book = require('../models/book');
+// var Author = require('../models/author');
+// var Author_Book = require('../models/author_book');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET home page. */
+router.get('/search', function(request, response, next) {
+  var search = request.query.search;
+  if (search) {
+    knex.search(search).then(function (data) {
+      response.render('search', {
+        search: search,
+        book: data.book,
+        author: data.author
+      });
+    });
+  } else {
+    response.render('index');
+  }
+});
+
+module.exports = router;
 // router.get(/movies/:id, (req, res) =>{
 // 	knex('movie')
 // 	.select('movie.title', 'actor.name')
@@ -19,4 +41,3 @@ router.get('/', function(req, res, next) {
 // 		})
 // 	})
 // })
-module.exports = router;
